f23614f4ccd802bf565d7d98111113aba7c03a83
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-07-13 20:17:50 +0100

    Add and configure Prettier with Husky and lint-staged

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,9 @@ import App from "./App";
 describe("App component", () => {
 	it("renders a form", () => {
 		render(<App />);
-		expect(screen.getByRole("textbox", { name: /first name/i })).toBeInTheDocument();
+		expect(
+			screen.getByRole("textbox", { name: /first name/i }),
+		).toBeInTheDocument();
 		expect(screen.getByRole("button", { name: /submit/i })).toBeInTheDocument();
 	});
 });
