c8b430200cf1825607eb85b704e3b44259a75d19
CircleCI <jonathan@codeyourfuture.io>
2022-07-31 09:28:11 +0000

    [skip ci] QA deploy to GitHub repo

---
+++ config.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-template-curly-in-string */
+(function (window) {
+	window.config = window.config || {};
+	window.config.API_URL = "https://corsproxy.io/?https%3A%2F%2Fcyf-api.qa.codeyourfuture.io";
+})(this);
